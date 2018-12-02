eae31401c10fff01056f772c5ffb5095c3d199b3
Tyler Wilding <xtvaser@gmail.com>
2018-12-02 01:48:48 -0500

    Day2: Completed

---
+++ day-2/src/day_2/core.clj
@@ -0,0 +1,50 @@
+(ns day-2.core
+  (:gen-class))
+
+(defn read-input [file]
+  (for [line (clojure.string/split-lines (slurp file))]
+    line))
+
+(defn not-empty? [coll]
+  (not (empty? coll)))
+
+(defn tally-occurences [init-list]
+  (loop [ids init-list
+         times-two 0
+         times-three 0]
+    (if ids
+      (let [id (first ids)
+            two-occurences (filter #(= % 2) (vals (frequencies id)))
+            three-occurences (filter #(= % 3) (vals (frequencies id)))]
+        (cond
+          (and (not-empty? two-occurences) (not-empty? three-occurences)) (recur (next ids) (inc times-two) (inc times-three))
+          (not-empty? two-occurences) (recur (next ids) (inc times-two) times-three)
+          (not-empty? three-occurences) (recur (next ids) times-two (inc times-three))
+          :else (recur (next ids) times-two times-three)))
+      [times-two times-three])))
+
+(defn part-one
+  [list]
+  (let [result (tally-occurences list)]
+    (* (first result) (second result))))
+
+(defn find-off-by-one
+  [list]
+  (for [i list
+        j list]
+    (let [set1 (into #{} (keys (frequencies i)))
+          set2 (into #{} (keys (frequencies j)))
+          diff (clojure.set/difference set1 set2)]
+      (if (= 1 (count diff))
+         [i diff]))))
+
+(defn part-two
+  [list]
+  (filter some? (find-off-by-one list)))
+
+; (read-input "resources/input")
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (part-one (read-input "resources/input"))
+  (part-two (read-input "resources/input")))
